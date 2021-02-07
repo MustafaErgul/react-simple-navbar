0e66bc896dace5a2de08973f4ea33dd8f05c1b16
mustafa ergul <mustafaergul0@gmail.com>
2021-02-07 16:18:44 +0300

    title align center

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ import imag from "../../public/images/apple.png";
 
 const About = () => (
   <div>
-    <h1 className="title is-1">This is the About Page</h1>
+    <h1 className="title">This is the About Page</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
       risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
   const { name } = useParams();
   return (
     <div>
-      <h1 className="title is-1">This is the Profile Page</h1>
+      <h1 className="title">This is the Profile Page</h1>
       <article className="message is-dark" style={{ marginTop: 40 }}>
         <div className="message-header">
           <p>{name}</p>
